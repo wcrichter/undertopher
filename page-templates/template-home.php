f15f5f02337c03513c9147b10cab67af436bbde3
Chris Richter <wcrichter@gmail.com>
2019-11-05 21:16:16 -0500

    Updated home hero and h1 style

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -2,7 +2,18 @@
 // Template Name: Home
 get_header();
 ?>
-
+<section id="section-home-hero">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>I’m Chris Richter ­— <br/>
+                    a Designer and Developer<br/>
+                    from Charleston, SC.
+                </h1>
+            </div>
+        </div>
+    </div>
+</section>
 <?php
 get_footer();
 ?>
